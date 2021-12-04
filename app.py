8ec9781e4cbe2127e5420518e5e7be4c19ae6b82
Jaee-C <77240633+Jaee-C@users.noreply.github.com>
2021-12-04 14:53:47 +0800

    added jinja templates for login and signup

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("login.html")
+    return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+    print(request.method)
+
+@app.route("/signup", methods=["GET"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    return redirect("/")
