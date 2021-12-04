edbd1c5f5576dd226e00d968cc752c7b97eec283
Jaee-C <77240633+Jaee-C@users.noreply.github.com>
2021-12-04 21:09:47 +0800

    can add movies to database, still cant display entries on screen

--- christmas/internal.py
+++ christmas/internal.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, redirect, render_template, request, session, url_for
 )
 
 from christmas.db import get_db
@@ -7,14 +7,40 @@ from christmas.db import get_db
 bp = Blueprint('internal', __name__, url_prefix='/api')
 
 @bp.route('/movie', methods=["GET", "POST"])
-def add_item():
-    if request.method == "GET":
-        return render_template("movie_update.html")
+def add_movie():
+    db = get_db()
     if request.method == "POST":
         movie = {}
         movie["title"] = request.form.get("movie-title")
         movie["description"] = request.form.get("movie-desc")
         movie["link"] = request.form.get("movie-link")
         movie["img"] = request.form.get("movie-img")
+
+        error = None
+
+        if not (movie["title"] and movie["description"] and movie["img"] and movie["link"]):
+            error = "Something is missing"
+
+        if error is None:
+            print(movie)
+            try:
+                db.execute(
+                    "INSERT INTO movies (title, description, img, external_link) VALUES (?, ?, ?, ?)",
+                    (movie["title"], movie["description"], movie["img"], movie["link"])
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"{movie['title']} is already used "
+            else:
+                return redirect(url_for("internal.add_movie"))
+
+
+        flash(error)
+
+    movies = db.execute("SELECT * FROM movies").fetchone()
+    print(movies)
+
+    return render_template("movie_update.html", movies=movies)
+            
     
     
