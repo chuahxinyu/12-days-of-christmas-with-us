87b5301aa3528b76ab8bab69d403f01d5b636370
Jaee-C <77240633+Jaee-C@users.noreply.github.com>
2021-12-04 17:17:29 +0800

    implemented login, register, and logout

--- christmas/auth.py
+++ christmas/auth.py
@@ -8,3 +8,78 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from christmas.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        db = get_db()
+        error = None
+
+        if not email:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif password != request.form.get("password-confirm"):
+            error = 'Passwords do not match.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (email, password) VALUES (?, ?)",
+                    (email, generate_password_hash(password))
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"{email} is already used"
+            else:
+                return redirect(url_for("auth.login"))
+        
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        db = get_db()
+        error = None()
+        user = db.execute(
+            'SELECT * FROM user WHERE email = ?', email
+        ).fetchone()
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user['password'], password):
+            error = "Incorrect password."
+        
+        if error is None:
+            session.clear()
+            session["user_id"] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+    
+    return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', user_id
+        ).fetchone()
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
